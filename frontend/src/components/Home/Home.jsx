12991aa9c78596ff1519331a264475a8b981d15f
Aayushi <ee200002001@iiti.ac.in>
2023-02-03 01:16:20 +0530

    Update Home.jsx

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,8 +1,134 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./Home.css";
+import * as THREE from "three";
+import moonImage from "../../Images/moon.jpg";
+import venusImage from "../../Images/venus.jpg";
+import spaceImage from "../../Images/space.jpg";
 
 const Home = () => {
+
+  useEffect(() => {
+    const textureLoader = new THREE.TextureLoader();
+
+    const moonTexture = textureLoader.load(moonImage);
+    const venusTexture = textureLoader.load(venusImage);
+    const spaceTexture = textureLoader.load(spaceImage);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
+
+    const canvas = document.querySelector(".homeCanvas");
+    console.log(canvas)
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    const moonGeometry = new THREE.SphereGeometry(2, 64, 64);
+    const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
+    const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+
+    const venusGeometry = new THREE.SphereGeometry(3, 64, 64);
+    const venusMaterial = new THREE.MeshBasicMaterial({ map: venusTexture });
+    const venus = new THREE.Mesh(venusGeometry, venusMaterial);
+    venus.position.set(8, 5, 5);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    const pointLight2 = new THREE.PointLight(0xffffff, 0.1);
+
+    pointLight.position.set(8, 5, 5);
+    pointLight2.position.set(-8, -5, -5);
+
+    scene.add(moon);
+    scene.add(venus);
+    scene.add(pointLight);
+    scene.add(pointLight2);
+    scene.background = spaceTexture;
+
+    const constSpeed = 0.01;
+    window.addEventListener("mousemove", (e) => {
+      if (e.clientX <= window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientX > window.innerWidth / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+
+      if (e.clientY > window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y += constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y += constSpeed;
+      }
+
+      if (e.clientY <= window.innerHeight / 2) {
+        moon.rotation.x -= constSpeed;
+        moon.rotation.y -= constSpeed;
+        venus.rotation.x -= constSpeed;
+        venus.rotation.y -= constSpeed;
+      }
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      moon.rotation.y += 0.001;
+      venus.rotation.y += 0.001;
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return window.addEventListener("scroll", () => {
+      camera.rotation.z = window.scrollY * 0.001;
+      camera.rotation.y = window.scrollY * 0.003;
+
+      const skillsBox = document.getElementById("homeskillsBox");
+
+      if (window.scrollY > 1500) {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOn";
+      } else {
+        skillsBox.style.animationName = "homeskillsBoxAnimationOff";
+      }
+    });
+  }, []);
+
   return (
-    <div>Home</div>
+    <>
+      <div className="home">
+        <canvas className="homeCanvas"></canvas>
+
+        <div className="homeCanvasContainer">
+          {/* <Typography variant="h1">
+            <p>A</p>
+            <p>B</p>
+            <p>H</p>
+            <p>I</p>
+            <p>S</p>
+            <p>H</p>
+            <p>E</p>
+            <p>K</p>
+          </Typography> */}
+
+          {/* <div className="homeCanvasBox">
+            <Typography variant="h2">DESIGNER</Typography>
+            <Typography variant="h2">DEVELOPER</Typography>
+            <Typography variant="h2">TEACHER</Typography>
+            <Typography variant="h2">CONTENT CREATOR</Typography>
+          </div> */}
+        </div>
+      </div>
+    </>
   )
 }
 
