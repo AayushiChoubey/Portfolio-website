50af49a1b9983f8a322e963c629b46daf1db4acd
Aayushi <ee200002001@iiti.ac.in>
2023-02-09 22:16:48 +0530

    Footer add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+//import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
+    <>
     <Router>
       <Header />
       <Routes>
 
         <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
+      <Footer />
     </Router>
+
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsGithub, BsYoutube, BsInstagram, BsLinkedin } from "react-icons/bs";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <Typography variant="h5">About Me</Typography>
+        <Typography>
+          Hey, my name is Aayushi Choubey. I am a Full-Stack Developer and a
+          student at <b> IIT Indore</b>
+        </Typography>
+
+        <Link to="/contact" className="footerContactBtn">
+          <Typography>Contact Us</Typography>
+        </Link>
+      </div>
+      <div>
+        <Typography variant="h6">Social Media</Typography>
+        <a href=" " target="black">
+          <BsGithub />
+        </a>
+        <a href="" target="black">
+          <BsYoutube />
+        </a>
+        <a href="" target="black">
+          <BsInstagram />
+        </a>
+        <a href="" target="black">
+          <BsLinkedin />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
