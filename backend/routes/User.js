3748b09f192384936380499eb1a076c5fb0a015d
Aayushi <ee200002001@iiti.ac.in>
2023-02-11 02:22:48 +0530

    Login, Logout and Getuser routes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,3 +1,14 @@
 import express from 'express';
 
+import {login,
+     logout, 
+     getUser}
+      from '../controllers/User.js';
+
 export const userRouter= express.Router();
+
+userRouter.route("/login").post(login);
+
+userRouter.route("/logout").get(logout);
+
+userRouter.route("/user").get(getUser);
