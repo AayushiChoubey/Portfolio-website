b2026bb2ac36289efa23ec8dc219395b01097249
Aayushi <ee200002001@iiti.ac.in>
2023-03-15 22:06:08 +0530

    AdminPanel completed

--- frontend/src/components/Admin/AdminPanel.jsx
+++ frontend/src/components/Admin/AdminPanel.jsx
@@ -1,13 +1,100 @@
-import { Typography } from '@mui/material'
-import React from 'react'
-import './AdminPanel.css'
+import React, { useEffect, useState } from "react";
+import "./AdminPanel.css";
+import { Button, Typography } from "@mui/material";
+import { AiOutlineProject } from "react-icons/ai";
+import { FaYoutube } from "react-icons/fa";
+import { MdTimeline } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, updateUser } from "../../actions/user";
+import { useAlert } from "react-alert";
 
 const AdminPanel = () => {
+  const dispatch = useDispatch();
+  const alert = useAlert();
+   const { message: loginMessage } = useSelector((state) => state.login ? state.login : {message: ""});
+   const { message, error, loading } = useSelector((state) => state.update ? state.update : {message: "", error: "", loading: false});
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [skills, setSkills] = useState({});
+  const [about, setAbout] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(updateUser(name, email, password, skills, about));
+
+    console.log(name, email, password, skills, about);
+  };
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  const handleAboutImage = (e) => {
+    const file = e.target.files[0];
+    const Reader = new FileReader();
+
+    Reader.readAsDataURL(file);
+
+    Reader.onload = () => {
+      if (Reader.readyState === 2) {
+        setAbout({ ...about, avatar: Reader.result });
+      }
+    };
+  };
+
+  const handleImages = (e, val) => {
+    const file = e.target.files[0];
+    const Reader = new FileReader();
+
+    Reader.readAsDataURL(file);
+
+    Reader.onload = () => {
+      if (Reader.readyState === 2) {
+        if (val === 1) {
+          setSkills({ ...skills, image1: Reader.result });
+        }
+        if (val === 2) {
+          setSkills({ ...skills, image2: Reader.result });
+        }
+        if (val === 3) {
+          setSkills({ ...skills, image3: Reader.result });
+        }
+        if (val === 4) {
+          setSkills({ ...skills, image4: Reader.result });
+        }
+        if (val === 5) {
+          setSkills({ ...skills, image5: Reader.result });
+        }
+        if (val === 6) {
+          setSkills({ ...skills, image6: Reader.result });
+        }
+      }
+    };
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch({ type: "CLEAR_ERRORS" });
+    }
+    if (message) {
+      alert.success(message);
+      dispatch({ type: "CLEAR_MESSAGE" });
+    }
+    if (loginMessage) {
+      alert.success(loginMessage);
+      dispatch({ type: "CLEAR_MESSAGE" });
+    }
+  }, [alert, error, message, dispatch, loginMessage]);
+
   return (
-    <div className='adminPanel'>
-      <div className='adminPanelContainer'>
-        <Typography variant='h4'>
-        <p>A</p>
+    <div className="adminPanel">
+      <div className="adminPanelContainer">
+        <Typography variant="h4">
+          <p>A</p>
           <p>D</p>
           <p>M</p>
           <p>I</p>
@@ -43,7 +130,7 @@ const AdminPanel = () => {
             className="adminPanelInputs"
           />
 
-<div className="adminPanelSkill">
+          <div className="adminPanelSkill">
             <div>
               <Typography>SKILL 1</Typography>
               <input
@@ -171,9 +258,9 @@ const AdminPanel = () => {
           <Button type="submit" variant="contained" disabled={loading}>
             Update
           </Button>
-          </form>
+        </form>
 
-          <Button
+        <Button
           variant="contained"
           color="error"
           style={{ display: "block", margin: "4vmax auto" }}
@@ -183,7 +270,7 @@ const AdminPanel = () => {
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPanel
+export default AdminPanel;
