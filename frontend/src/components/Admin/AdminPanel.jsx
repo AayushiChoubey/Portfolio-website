a5777bdfd824384f61d35ba6c5ffb8bf3c7ba051
Aayushi <ee200002001@iiti.ac.in>
2023-03-15 11:35:36 +0530

    AdminPanel structure

--- frontend/src/components/Admin/AdminPanel.jsx
+++ frontend/src/components/Admin/AdminPanel.jsx
@@ -1,8 +1,188 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+import './AdminPanel.css'
 
 const AdminPanel = () => {
   return (
-    <div>AdminPanel</div>
+    <div className='adminPanel'>
+      <div className='adminPanelContainer'>
+        <Typography variant='h4'>
+        <p>A</p>
+          <p>D</p>
+          <p>M</p>
+          <p>I</p>
+          <p style={{ marginRight: "1vmax" }}>N</p>
+
+          <p>P</p>
+          <p>A</p>
+          <p>N</p>
+          <p>E</p>
+          <p>L</p>
+        </Typography>
+
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="adminPanelInputs"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="adminPanelInputs"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="adminPanelInputs"
+          />
+
+<div className="adminPanelSkill">
+            <div>
+              <Typography>SKILL 1</Typography>
+              <input
+                className="adminPanelFileUpload"
+                type="file"
+                onChange={(e) => handleImages(e, 1)}
+                accept="image/*"
+              />
+            </div>
+            <div>
+              <Typography>SKILL 2</Typography>
+
+              <input
+                type="file"
+                onChange={(e) => handleImages(e, 2)}
+                accept="image/*"
+                className="adminPanelFileUpload"
+              />
+            </div>
+            <div>
+              <Typography>SKILL 3</Typography>
+
+              <input
+                type="file"
+                onChange={(e) => handleImages(e, 3)}
+                accept="image/*"
+                className="adminPanelFileUpload"
+              />
+            </div>
+            <div>
+              <Typography>SKILL 4</Typography>
+
+              <input
+                type="file"
+                onChange={(e) => handleImages(e, 4)}
+                accept="image/*"
+                className="adminPanelFileUpload"
+              />
+            </div>
+            <div>
+              <Typography>SKILL 5</Typography>
+
+              <input
+                type="file"
+                onChange={(e) => handleImages(e, 5)}
+                accept="image/*"
+                className="adminPanelFileUpload"
+              />
+            </div>
+            <div>
+              <Typography>SKILL 6</Typography>
+
+              <input
+                type="file"
+                onChange={(e) => handleImages(e, 6)}
+                accept="image/*"
+                className="adminPanelFileUpload"
+              />
+            </div>
+          </div>
+
+          <div className="adminPanelAbout">
+            <fieldset>
+              <legend>About</legend>
+              <input
+                type="text"
+                placeholder="Name"
+                value={about.name}
+                onChange={(e) => setAbout({ ...about, name: e.target.value })}
+                className="adminPanelInputs"
+              />
+              <input
+                type="text"
+                placeholder="Title"
+                value={about.title}
+                onChange={(e) => setAbout({ ...about, title: e.target.value })}
+                className="adminPanelInputs"
+              />
+              <input
+                type="text"
+                placeholder="Subtitle"
+                value={about.subtitle}
+                onChange={(e) =>
+                  setAbout({ ...about, subtitle: e.target.value })
+                }
+                className="adminPanelInputs"
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                value={about.description}
+                onChange={(e) =>
+                  setAbout({ ...about, description: e.target.value })
+                }
+                className="adminPanelInputs"
+              />
+              <input
+                type="text"
+                placeholder="Quote"
+                value={about.quote}
+                onChange={(e) => setAbout({ ...about, quote: e.target.value })}
+                className="adminPanelInputs"
+              />
+
+              <input
+                type="file"
+                onChange={handleAboutImage}
+                className="adminPanelFileUpload"
+                placeholder="Choose Avatar"
+                accept="image/*"
+              />
+            </fieldset>
+          </div>
+
+          <Link to="/admin/timeline">
+            TIMELINE <MdTimeline />
+          </Link>
+          <Link to="/admin/youtube">
+            YOUTUBE <FaYoutube />
+          </Link>
+          <Link to="/admin/project">
+            PROJECTS <AiOutlineProject />
+          </Link>
+
+          <Button type="submit" variant="contained" disabled={loading}>
+            Update
+          </Button>
+          </form>
+
+          <Button
+          variant="contained"
+          color="error"
+          style={{ display: "block", margin: "4vmax auto" }}
+          onClick={logoutHandler}
+        >
+          LOGOUT
+        </Button>
+      </div>
+    </div>
   )
 }
 
